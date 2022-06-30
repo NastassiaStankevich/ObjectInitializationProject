15f85d8d598497b49c4bbd489bee86a198b25e50
Paxan58 <pavelstaricov318@mail.ru>
2022-06-30 19:56:38 +0300

    Initial commit

---
+++ src/Student.java
@@ -0,0 +1,14 @@
+public class Student {
+    public String name;
+    public int age;
+    public double mark;
+    public boolean alive;
+
+    public Student(){
+        name = "no name";
+        age = 16;
+        mark = 4;
+        alive = true;
+    }
+
+}
---
+++ src/Task02.java
@@ -0,0 +1,16 @@
+public class Task02 {
+    public static void main(String[] args) {
+        Student st = new Student();
+        st.name = "Alex";
+        st.age = 20;
+
+        Student st2 = new Student();
+        st2.name = "Anna";
+        st2.age = 18;
+
+        Student st3 = new Student();
+        st3.name = "Peter";
+        st3.age = 21;
+ 
+    }
+}
