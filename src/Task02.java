67499dc5b4d074b2232a43e1dff0f813e1473f43
Paxan58 <pavelstaricov318@mail.ru>
2022-06-30 20:07:46 +0300

    Add getInfo Method into Student class

--- src/Task02.java
+++ src/Task02.java
@@ -3,18 +3,22 @@ public class Task02 {
         Student st = new Student();
         st.name = "Alex";
         st.age = 20;
-        System.out.println(st.name);
-        System.out.println(st.age);
-        System.out.println(st.mark);
-        System.out.println(st.alive);
+//        System.out.println(st.name);
+//        System.out.println(st.age);
+//        System.out.println(st.mark);
+//       System.out.println(st.alive);
+
+        System.out.println(st.getInfo());
 
         Student st2 = new Student();
         st2.name = "Anna";
         st2.age = 18;
+        System.out.println(st2.getInfo());
 
         Student st3 = new Student();
         st3.name = "Peter";
         st3.age = 21;
+        System.out.println(st3.getInfo());
 
     }
 }
