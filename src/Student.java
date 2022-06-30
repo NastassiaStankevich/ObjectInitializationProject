308b58a6894e87eed749e1ca4f67b35613662d32
Paxan58 <pavelstaricov318@mail.ru>
2022-06-30 21:21:15 +0300

    alt + insert - constructor

--- src/Student.java
+++ src/Student.java
@@ -4,13 +4,36 @@ public class Student {
     public double mark = 4;
     public boolean alive;
 
-    // default constructor
-    public Student(){
-        name = "no name";
-        age = 16;
-        mark = 4;
-        alive = true;
+    public Student(String name, int age, double mark, boolean alive) {
+        this.name = name;
+        this.age = age;
+        this.mark = mark;
+        this.alive = alive;
     }
+
+    // default constructor
+//    public Student(){
+//        name = "no name";
+//        age = 16;
+//        mark = 4;
+//        alive = true;
+//    }
+
+    // Full constructor with params/args
+//    public  Student(String name, int age , double mark , boolean alive){
+//        this.name = name;
+//        this.age = age;
+//        this.mark = mark;
+//        this.alive = alive;
+//
+//    }
+    // copy constructor
+//    public Student(Student student){
+//        name = student.name;
+//        age = student.age;
+//        mark = student.mark;
+//        alive = student.alive;
+//    }
 
     public String getInfo(){
         return  name + ": age = " + age
@@ -18,21 +41,4 @@ public class Student {
                 + ", is alive = " + (alive ? "yes" : "No");
 
     }
-    // Full constructor with params/args
-    public  Student(String name, int age , double mark , boolean alive){
-        this.name = name;
-        this.age = age;
-        this.mark = mark;
-        this.alive = alive;
-
-    }
-    // copy constructor
-    public Student(Student student){
-        name = student.name;
-        age = student.age;
-        mark = student.mark;
-        alive = student.alive;
-    }
-
-
 }
